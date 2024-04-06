b08adb630775456ca4d8e079ec4b90d4086c4694
Mohssine Belhamra <mohssine.belhamra@etu.univ-amu.fr>
2024-04-06 23:32:52 +0200

    Added the main completed files of my portfolio code

---
+++ main.js
@@ -0,0 +1,149 @@
+
+
+
+
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+
+if (navToggle){
+    navToggle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+        
+
+    })
+}
+if (navClose){
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+        
+
+
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+const navLink = document.querySelectorAll('.nav__link')
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click',linkAction))
+
+
+
+
+
+
+
+
+
+
+const blurHeader = () =>{
+    const header = document.getElementById('blur-header')
+    this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
+}
+
+window.addEventListener('scroll',blurHeader);
+
+
+
+
+
+
+
+
+
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message');
+const sendEmail = (e) =>{
+    e.preventDefault()
+    
+    emailjs.sendForm('service_79k4qh9','template_9fin76t','#contact-form','bPPyCFqi56RW1ZD1F').then(() =>{
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        setTimeout(()=>{
+            contactMessage.textContent = ''
+        }, 3000);
+        contactForm.reset();
+    },() =>{
+        contactMessage.textContent = 'Message not sent ( service error) ❌'
+    });
+
+}
+contactForm.addEventListener('submit', sendEmail);
+
+
+
+
+
+
+
+
+
+
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up');
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll'):scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll',scrollUp);
+
+
+
+
+
+
+
+
+
+
+
+const sections = document.querySelectorAll('section[id]');
+
+const scrollActive = () => {
+    const scrollY = window.pageYOffset;
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionClass.classList.add('active-link');
+        } else {
+            if (sectionClass) {
+                sectionClass.classList.remove('active-link');
+            }
+        }
+    });
+}
+
+window.addEventListener('scroll', scrollActive);
+
+
+
+
+
+
+const sr = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:'2500',
+    delay:400,
+})
+sr.reveal(`.home__data,.home__social,.contact__container,.footer__container`)
+sr.reveal(`.home__image`,{origin: 'bottom'})
+sr.reveal(`.about__data, .skills__data`,{origin: 'left'})
+sr.reveal(`.about__image, .skills__content`,{origin: 'right'})
+sr.reveal(`.education__card, .projects__card`,{interval:100})
